2fd87aadfe909aaab7adc0edce9d2c6971ad0996
ascsl <jose@ascsl.com>
2018-10-19 20:36:26 +0200

    Create TraitArrayConjuntos.php

---
+++ Traits/TraitArrayConjuntos.php
@@ -0,0 +1,15 @@
+<?php
+
+	// Permite realizar operaciones con arrays como si fueran Conjuntos
+
+/*
+	// Propiedades:
+
+	// Metodos:
+	public function arrayUnion($conjA=[], $conjB=[]) {
+	public function arrayInterseccion($conjA=[], $conjB=[]) {
+	public function arrayDiferencia($conjA=[], $conjB=[]) {
+	public function arrayComplemento($conjA=[], $conjB=[]) {
+*/
+trait TraitArrayConjuntos {
+}
